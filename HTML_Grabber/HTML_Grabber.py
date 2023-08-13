45be29a4f9aac9600a62c54676c3f07aaad75725
Josh <54005918+Jman32411@users.noreply.github.com>
2023-08-12 20:35:18 -0400

    Overhauled the HTML reading to search based off of file location. Added a test Hamilton HTML page.
    Changed:
    -removed urllib completely
    -takes and searches through a pre-set file location
    -modifies the string data so it can be directly put into a spreadsheet
    -added a test HTML page
    
    TO DO:
    -expand to NOT JUST the party/attorney info (the case summary info too)
    -export data to excel sheet
    -test more pages

--- HTML_Grabber/HTML_Grabber.py
+++ HTML_Grabber/HTML_Grabber.py
@@ -1,10 +1,22 @@
-from urllib.request import *
-def getHTML(url):
-    page = urlopen(url) #Store the URL
-    html_bytes = page.read() #Explanation here -> https://docs.python.org/3/library/urllib.request.html#examples
-    html = html_bytes.decode("utf-8") 
-    return(html)
+import linecache
+import re
+#The "r" is neccesary because it converts a normal string to a raw string. See https://stackoverflow.com/questions/37400974/error-unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3
+path = r"C:\Users\profs\Desktop\Joshua\CaseScraper\CaseScraper\HTML_Grabber\Hamilton County Clerk of Courts.html" 
 
-print("START \n \n")
-print(getHTML("https://www.courtclerk.org/data/case_summary.php?sec=party&casenumber=23CV16448&submit.x=23&submit.y=21"))
-print("\n \n END")
+#https://pynative.com/python-search-for-a-string-in-text-files/#:~:text=Open%20a%20file%20in%20a,current%20line%20and%20line%20number.
+def searchHTML(path, searchFor):
+    with open(path, 'r') as filePath:
+        for lineNumber, line in enumerate(filePath):
+            if str(searchFor) in line:
+                return(lineNumber)
+                break
+
+def removeHTMLFormatting(line):
+    return(re.sub("[</td>&nbspr;]", "", line))
+
+
+#"aria-live" is the indicator that I used since that's right above where the party/attourney info is
+#https://stackoverflow.com/a/16432254
+for num in range(11):
+    if num != 7:
+        print(removeHTMLFormatting(str(linecache.getline(path, searchHTML(path, "aria-live") + num + 3))))
