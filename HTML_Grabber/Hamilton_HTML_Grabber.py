c2f1a2134424376e4e820d6228643ec7387d9212
Josh <54005918+Jman32411@users.noreply.github.com>
2023-08-14 11:46:06 -0400

    Added a new case to test with
    Changed:
    -Outputs should FINALLY be fixed
    -Now (hopefully) works with multiple files
    -Can (but currently disabled) go through and read all files in specified directory
    -Can handle most missing keywords
    
    To Do:
    -Export to Excel
    -MORE TESTING!
    -If possible, clean up more

--- HTML_Grabber/Hamilton_HTML_Grabber.py
+++ HTML_Grabber/Hamilton_HTML_Grabber.py
@@ -1,21 +1,25 @@
 import linecache
 import re
 import pandas as pd
+import os
 from openpyxl import load_workbook
 from datetime import date
 
 #The "r" is neccesary because it converts a normal string to a raw string. See https://stackoverflow.com/questions/37400974/error-unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3
-readPath = r"C:\Users\profs\Desktop\Joshua\CaseScraper\CaseScraper\HTML_Grabber\Hamilton County Clerk of Courts.html" #I'm using Case# 23CV17209 for testing
+readPath = r"C:\Users\profs\Desktop\Joshua\CaseScraper\CaseScraper\HTML_Grabber\Hamilton County Clerk of Courts " #I'm using Case# 23CV17209 for testing
 writePath = r"C:\Users\profs\Desktop\Joshua\CaseScraper\CaseScraper\HTML_Grabber\Hamilton County Clerk of Courts Output.xlsx"
 companyName = "" #PLACEHOLDER
 beginDate = "" #PLACEHOLDER
 endDate = "" #PLACEHOLDER
-caseKeywords = ["Case Number:", "Court:", "Case Caption:", "Judge:", "Filed Date:", "Case Type", "Amount:", "Bannanna:"] #"Bannanna:" was/is used to test for unfindable keywords
+caseKeywords = ["Case Number:", "Court:", "Case Caption:", "Judge:", "Filed Date:", "Case Type", "Nut", "Amount:", "Bannanna:"] #"Nut" and "Bannanna:" was/is used to test for unfindable keywords
 partyCaseKeywords = ["Plaintiff Name", "Plaintiff Address", "Party", "Attorney", "Attorney Address", "Court ID", "Defendant Name", "Defendant Address", "Defendant Party"]
 data = {}
 
 def createExcelSheet(caseKeywords, partyCaseKeywords, writePath):
     #Source: https://saturncloud.io/blog/how-to-append-a-pandas-dataframe-to-an-excel-sheet-a-comprehensive-guide/
+    #https://sparkbyexamples.com/pandas/pandas-write-to-excel-with-examples/#:~:text=Use%20pandas%20to_excel()%20function,sheet%20name%20to%20write%20to.
+    #https://pythonbasics.org/write-excel/
+    #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_excel.html
     #Adds new dictionary keys for each case Keyword
     for keyword in range(0, len(partyCaseKeywords)):
         data[re.sub('[:]', "", partyCaseKeywords[keyword])] = []
@@ -51,6 +55,7 @@ def harvestData(readPath, caseKeywords, partyCaseKeywords):
             data[partyCaseKeywords[num]].append((re.sub("[;/]", " ", (re.sub("[<td>&nbspr\n]", "", (str(linecache.getline(readPath, searchHTML(readPath, "aria-live") + num + linebreak + 4))))))))
         else:
             linebreak = 1
+            data[partyCaseKeywords[num]].append((re.sub("[;/]", " ", (re.sub("[<td>&nbspr\n]", "", (str(linecache.getline(readPath, searchHTML(readPath, "aria-live") + num + 5))))))))
 
 
 
@@ -58,9 +63,10 @@ createExcelSheet(caseKeywords, partyCaseKeywords, writePath)
 print("\n" + "Created (but empty) Excel Sheet: ")
 for key, value in data.items():
     print(key, value)
-
 print("\n" + "Harvested Data: ")
-harvestData(readPath, caseKeywords, partyCaseKeywords)
+#for number in range(1, len(os.listdir(r"C:\Users\profs\Desktop\Joshua\CaseScraper\CaseScraper\HTML_Grabber")) + 1):
+for number in range(1, 2 + 1):  # this is used since the current directory has other files that'll break the for loop
+    harvestData(readPath + str(number) + ".html", caseKeywords, partyCaseKeywords)
 for key, value in data.items():
     print(key, value)
 
