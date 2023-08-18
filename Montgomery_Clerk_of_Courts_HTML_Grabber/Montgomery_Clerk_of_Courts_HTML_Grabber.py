dd27a073dc6b74be7e672f0eb7743f5905385b68
Josh <54005918+Jman32411@users.noreply.github.com>
2023-08-17 23:11:23 -0400

    Cleaned up Montgomery Code
    Changed:
    -Cleaned up Montgomery code
    
    To do:
    -Test Montgomery and Hamilton
    -Bugfix (if needed) Montgomery and Hamilton
    -Clean up code

--- Montgomery_Clerk_of_Courts_HTML_Grabber/Montgomery_Clerk_of_Courts_HTML_Grabber.py
+++ Montgomery_Clerk_of_Courts_HTML_Grabber/Montgomery_Clerk_of_Courts_HTML_Grabber.py
@@ -43,24 +43,7 @@ caseSummaryKeywords = ["PLAINTIFF", "DEFENDANT", "File Date:", "Nuts", "Court:",
 casePartyKeywords = ["Plaintiff Address", "Attorney", "Nuts", "Defendant Address", "Bananas:"]
 data = {}
 
-# testString = "120340567089"
-# print(testString[1:])
-# print(testString[-1:])
-# print(testString[:1])
-# print(testString[:-1])
-# print(testString.find("0"))
-# testValue = 4/9
-# print(testValue)
-# testValue = round(testValue)
-# print(testValue)
-# print(keyDictionary["Plaintiff Name"])
-# print(list(keyDictionary.keys())[list(keyDictionary.values()).index("PLAINTIFF")])
-
-# print("\n")
-
-
 def createDictionary(Keys):
-    print("\nStart createDictionary:")
     #Adds new dictionary keys for each case Keyword
     for keyword in Keys:
         data[keyword] = []
@@ -72,7 +55,6 @@ def searchHTML(readPath, identifierString):
                 return(lineNumber + 1)
 
 def makeLine(filePath, lineNum):
-    # print("\nStart makeLine:")
     linebreak = 0
     while str(linecache.getline(filePath, lineNum + linebreak)[-2]) != "=":
         linebreak += 1
@@ -83,76 +65,51 @@ def makeLine(filePath, lineNum):
     return(line)
 
 def harvestData(readPath, filenum):
-    print("\nStart harvest Data:")
     switchPage = "Party"
     filePath = readPath + switchPage + " " + str(filenum) + ".mhtml"
     lineNum = searchHTML(filePath, "caseInfo_" + switchPage)
     line = makeLine(readPath + switchPage + " " + str(filenum) + ".mhtml", lineNum)
-    # print(list(keyDictionary.values())[0])
     for keyword in list(keyDictionary.values()):
         if list(keyDictionary.values())[9] == keyword:
             switchPage = "Summary"
             filePath = readPath + switchPage + " " + str(filenum) + ".mhtml"
             lineNum = searchHTML(filePath, "CaseInf")
             line = makeLine(readPath + switchPage + " " + str(filenum) + ".mhtml", lineNum)
-            print("switched")
-        print("|")
-        print(keyword)
-        print(int(line.find(keyword)))
         if line.find(keyword) != -1:
             if switchPage == "Party":
                 if keyword == "PLAINTIFF" or keyword == "DEFENDANT":
                     line = line[int(line.find("<strong>")) + 8:]
                     data[list(keyDictionary.keys())[list(keyDictionary.values()).index(keyword)]].append(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("</strong>"))])))
-                    print(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("</strong>"))])))
-                    # data[partyCaseKeywords[num]].append(
-                    print("|\n")
                 elif keyword == "Attorney:":
                     line = line[int(line.find(keyword)) + len(keyword) + 13:]
                     data[list(keyDictionary.keys())[list(keyDictionary.values()).index(keyword)]].append(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("<br>"))])))
-                    print(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("<br>"))])))
-                    print("|\n")
                 else:
                     line = line[int(line.find(keyword)) + len(keyword) + (round(len(keyword)/9) * 13):]
                     data[list(keyDictionary.keys())[list(keyDictionary.values()).index(keyword)]].append(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("</div>"))])))
-                    print(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("</div>"))])))
-                    print("|\n")
             else:
                 if keyword == "CaseInf":
                     lineNum = searchHTML(filePath, keyword)
                     line = makeLine(readPath + switchPage + " " + str(filenum) + ".mhtml", lineNum)
                     line = line[int(line.find("CaseInfo")) + 9:]
                     data[list(keyDictionary.keys())[list(keyDictionary.values()).index(keyword)]].append(re.sub("[b]", " ", re.sub("[<r&amp;span/>]", "",line)))
-                    print(re.sub("[b]", " ", re.sub("[<r&amp;span/>]", "",line)))
 
                     lineNum = searchHTML(filePath, "caseInfo_" + switchPage)
                     line = makeLine(readPath + switchPage + " " + str(filenum) + ".mhtml", lineNum)
-                    print("|\n")
                 else:
                     line = line[int(line.find(keyword)) + len(keyword) + 9:]
                     data[list(keyDictionary.keys())[list(keyDictionary.values()).index(keyword)]].append(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("</td>"))])))
-                    print(re.sub("[b]", " ", re.sub("[<r&amp;>ion/thedy]", "",line[:int(line.find("</td>"))])))
-                    print("|\n")
         else:
             if keyword == "PLAINTIFF":
                 break
             data[list(keyDictionary.keys())[list(keyDictionary.values()).index(keyword)]].append("")
-            print("|\n")
 
 def exportToExcel(data, writePath, countyName, startDate, endDate):
     dataFrame = pd.DataFrame(data)
     dataFrame.to_excel(str(re.sub("[ ]", "", writePath)) + countyName + " " + str(startDate) + " to " + str(endDate) + ".xlsx")
 
 createDictionary(keyDictionary)
-# filenum = "6"
-# partyOrSummary = "Party"
-# filePath = readPath + partyOrSummary + " " + str(filenum) + ".mhtml"
-# lineNum = searchHTML(filePath, "caseInfo_" + partyOrSummary)
-# harvestData(readPath, filenum)
-# print(data)
 for number in range(1, int((len(os.listdir(readFolder)) / 2)) + 1):
     harvestData(readPath, number)
-print(data)
 exportToExcel(data, writePath, countyName, startDate, str(endDate))
 
 print("Process finished --- %s seconds ---" % (time.time() - start_time))
